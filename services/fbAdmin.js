92ea8274310b72bf8f856be0b93bd816096b799c
Daniel <danstraw86@gmail.com>
2021-04-03 07:16:20 -0700

    updated env variable c

--- services/fbAdmin.js
+++ services/fbAdmin.js
@@ -6,7 +6,12 @@ dotenv.config({
   path: path.resolve(__dirname, '../config/.env')
 });
 
-var serviceAccount = process.env.SERVICE_ACCOUNT_KEY || require('../serviceAccountKey.json');
+var serviceAccount = {
+  "type": process.env.FIREBASE_PROJECT_TYPE,
+  "projectId": process.env.FIREBASE_PROJECT_ID,
+  "private_key": process.env.FIREBASE_PRIVATE_KEY,
+  "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+} || require('../serviceAccountKey.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
